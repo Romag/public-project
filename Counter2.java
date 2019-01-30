1b002a73b5b02cc2b9aeb2cb4fc1701cf18719b0
Ramil Magyerramov <rmagerramov11@gmail.com>
2019-01-30 11:36:12 +0200

    TEST-2 Remove single and add double and triple
    Remove single counter method
    Add double and triple counter methods for new and exciting uses.

--- Counter2.java
+++ Counter2.java
@@ -1,7 +1,11 @@
 public class Counter {
   private int counter = 0;
   
-  public int count() {
-    return counter++;
+  public int doubleCount() {
+    return counter+=2;
+  }
+  
+  public int tripleCount() {
+    return counter+=3;
   }
 }
