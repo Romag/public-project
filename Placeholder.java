888ef426e63d4539e8b5ea8720cd69d112a2a5f2
Ramil Magyerramov <rmagerramov11@gmail.com>
2019-01-31 16:01:09 +0200

    TEST-2 This is some randomly composed long text created with sole purpose of upsetting Github (and test table formatting despite the fact that lon short messages are strongly discouraged and doesn't make sense, but we all know true nature of users, who only desire to break code and make developers's life as miserable as possible).

---
+++ Placeholder.java
@@ -0,0 +1,15 @@
+public class Placeholder {
+  String s = "I" +
+  "decided" +
+  "to" +
+  "add" +
+  "some" +
+  "really" +
+  "long" +
+  "text" +
+  "into" +
+  "file" +
+  "for" +
+  "testin" +
+  "purposes";
+}
