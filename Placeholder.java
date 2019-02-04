09375ce31a6add5985a14447a4cde3597fad68af
Ramil Magyerramov <rmagerramov11@gmail.com>
2019-02-04 11:33:24 +0200

    TEST-2
    Make changes in two parts to check diff hunks

--- Placeholder.java
+++ Placeholder.java
@@ -2,11 +2,13 @@ public class Placeholder {
   String s = "I" +
   "decided" +
   "to" +
+  "rofl" +
   "add" +
   "some" +
   "really" +
   "long" +
   "text" +
+  "rofl2" +
   "into" +
   "file" +
   "for" +
